6f57ac710f3d08fe092e2b8bf509ac517cdcba27
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-07-29 08:17:50 +0530

    Determine If Two String are Close

---
+++ DetermineIfTwoStringareClose.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if(word1.size() != word2.size()){
+            return false;
+        }
+        vector<int> c1(26, 0);
+        vector<int> c2(26, 0);
+        for(int i = 0; i < word1.size(); i++){
+            c1[word1[i] - 'a']++;
+            c2[word2[i] - 'a']++;
+        }
+        for(int i = 0; i < 26; i++){
+            if(c2[i] == 0 && c1[i] != 0) return false;
+        }
+        sort(c1.begin(), c1.end());
+        sort(c2.begin(), c2.end());
+        for(int i = 0; i < 26; i++){
+            if(c1[i] != c2[i]) return false;
+        }
+        return true;
+    }
+};
